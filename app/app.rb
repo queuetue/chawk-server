338099a323ff9c32144b253cc05ef4b4041ce5aa
Scott Russell <queuetue@gmail.com>
2014-03-17 11:04:10 -0400

    Openauthw only sends refresh token the first auth.  This need smore consideration.

--- app/app.rb
+++ app/app.rb
@@ -147,11 +147,6 @@ module Chawk
 			new_token = client.auth_code.get_token(params[:code], :redirect_uri => g_redirect_uri)
 			session[:access_token]  = new_token.token
 			session[:refresh_token] = new_token.refresh_token
-
-			if session[:access_token].nil? or session[:refresh_token].nil?
-				raise "??????????????????? #{new_token.token} - #{new_token.refresh_token}"
-			end
-
 			redirect '/'
 		end
 
